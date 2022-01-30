2f95bb1eec97f5bf7673dce6e057b3709a3fe9e0
darkopopovski <darkopopovski39@yahoo.com>
2022-01-30 12:41:47 +0100

    Fixing Seats and Reservations #1

--- src/main/java/com/example/demo/controller/MovieProjectionController.java
+++ src/main/java/com/example/demo/controller/MovieProjectionController.java
@@ -1,8 +1,12 @@
 package com.example.demo.controller;
 
-import com.example.demo.model.IS_.MovieGenreCombination;
+import com.example.demo.model.Auditorium;
+import com.example.demo.model.Gives.Gives;
+import com.example.demo.model.Gives.GivesKey;
 import com.example.demo.model.Movie;
 import com.example.demo.model.MovieProjection;
+import com.example.demo.repository.AuditoriumRepository;
+import com.example.demo.repository.GivesRepository;
 import com.example.demo.repository.MovieGenreCombinationRepository;
 import com.example.demo.service.MovieProjectionService;
 import com.example.demo.service.MovieService;
@@ -19,10 +23,14 @@ public class MovieProjectionController {
     private final MovieProjectionService movieProjectionService;
     private final MovieService movieService;
     private final MovieGenreCombinationRepository movieGenreCombinationRepository;
-    public MovieProjectionController(MovieProjectionService movieProjectionService, MovieService movieService, MovieGenreCombinationRepository movieGenreCombinationRepository) {
+    private final AuditoriumRepository auditoriumRepository;
+    private final GivesRepository givesRepository;
+    public MovieProjectionController(MovieProjectionService movieProjectionService, MovieService movieService, MovieGenreCombinationRepository movieGenreCombinationRepository, AuditoriumRepository auditoriumRepository, GivesRepository givesRepository) {
         this.movieProjectionService = movieProjectionService;
         this.movieService = movieService;
         this.movieGenreCombinationRepository = movieGenreCombinationRepository;
+        this.auditoriumRepository = auditoriumRepository;
+        this.givesRepository = givesRepository;
     }
 
     @GetMapping
@@ -40,7 +48,9 @@ public class MovieProjectionController {
     @GetMapping("/add-form")
     public String addProjectionPage(Model model) {
         List<Movie> movie = this.movieService.findAll();
+        List<Auditorium> auditorium = this.auditoriumRepository.findAll();
         model.addAttribute("movie", movie);
+        model.addAttribute("auditorium", auditorium);
         model.addAttribute("bodyContent", "add-projection");
         return "master-template";
     }
@@ -53,8 +63,19 @@ public class MovieProjectionController {
             @RequestParam String projection_screening_date,
             @RequestParam String projection_type,
             @RequestParam Float projection_price,
-            @RequestParam Integer movie_id) {
+            @RequestParam Integer movie_id,
+            @RequestParam Integer auditorium_id) {
             this.movieProjectionService.save(projection_id, projection_movie_start, projection_movie_end, projection_screening_date, projection_type, projection_price, movie_id);
+
+            Gives gives = new Gives();
+            GivesKey givesKey = new GivesKey();
+            MovieProjection movieProjection = this.movieProjectionService.findById(projection_id).get();
+            Auditorium auditorium = this.auditoriumRepository.findById(auditorium_id).get();
+            givesKey.setMovieProjection(movieProjection);
+            givesKey.setAuditorium(auditorium);
+            gives.setGivesKey(givesKey);
+            gives.setSeat_limitation(35);
+            this.givesRepository.save(gives);
             return "redirect:/projections";
     }
 }
